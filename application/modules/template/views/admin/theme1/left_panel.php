7002908b51dae0f76c10a6f70901e0bc815f3764
ahmadsiddiquech <muhammad.ahmad@xpertspot.com>
2019-09-19 17:57:52 +0500

    filtered data

--- application/modules/template/views/admin/theme1/left_panel.php
+++ application/modules/template/views/admin/theme1/left_panel.php
@@ -13,7 +13,6 @@
           <!-- Iterates over all sidebar items-->
 
       
-      <?php if($role_id!=1){ ?>
          <li class="<?php if($curr_url == 'dashboard'){echo 'active';}    ?>">
                 <a href="<?php $controller='dashboard'; 
                    echo ADMIN_BASE_URL . $controller ?>">
@@ -21,19 +20,18 @@
                    <span>Dashboard</span>
                 </a>
           </li>
-          <?php } if($role_id==1){ ?>
-          <li class="<?php if($curr_url == 'organizations'){echo 'active';}    ?>">
-              <a href="<?php $controller='organizations'; 
+          <li class="<?php if($curr_url == 'cashier'){echo 'active';}    ?>">
+              <a href="<?php $controller='cashier'; 
               echo ADMIN_BASE_URL . $controller ?>">
              <em class="fa fa-users"></em>
-                <span>Organizations</span>
+                <span>Cashier</span>
              </a>
           </li>
-          <li class="<?php if($curr_url == 'banner'){echo 'active';}    ?>">
-                <a href="<?php $controller='banner'; 
+          <li class="<?php if($curr_url == 'customer'){echo 'active';}    ?>">
+                <a href="<?php $controller='customer'; 
                    echo ADMIN_BASE_URL . $controller ?>">
                    <em class="fa fa-flag"></em>
-                   <span>Banner</span>
+                   <span>Customer</span>
                 </a>
           </li>
           <li class="<?php if($curr_url == 'user_log'){echo 'active';}    ?>">
@@ -43,98 +41,6 @@
                    <span>User Log</span>
                 </a>
           </li>
-         <?php } if($role_id != 1){ ?>
-          <li class="<?php if($curr_url == 'user'){echo 'active';}    ?>">
-                <a href="<?php $controller='user'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-user"></em>
-                   <span>User</span>
-                </a>
-          </li>
-
-          <li class="<?php if($curr_url == 'program'){echo 'active';}    ?>">
-                <a href="<?php $controller='program'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-th-large"></em>
-                   <span>Program</span>
-                </a>
-          </li>
-          <li class="<?php if($curr_url == 'program'){echo 'active';}    ?>">
-                <a href="<?php $controller='program'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-th-large"></em>
-                   <span>Program</span>
-                </a>
-          </li>
-
-
-          <li class="<?php if($curr_url == 'classes'){echo 'active';}    ?>">
-                <a href="<?php $controller='classes'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-th-list"></em>
-                   <span>Classes</span>
-                </a>
-          </li>
-
-          <li class="<?php if($curr_url == 'sections'){echo 'active';}    ?>">
-                <a href="<?php $controller='sections'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-delicious"></em>
-                   <span>Sections</span>
-                </a>
-          </li>
-
-          <li class="<?php if($curr_url == 'subjects'){echo 'active';}    ?>">
-                <a href="<?php $controller='subjects'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-book"></em>
-                   <span>Subjects</span>
-                </a>
-          </li>
-
-          <li class="<?php if($curr_url == 'student'){echo 'active';}    ?>">
-                <a href="<?php $controller='student'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-child"></em>
-                   <span>Student</span>
-                </a>
-          </li>
-          <li class="<?php if($curr_url == 'exam'){echo 'active';}    ?>">
-                <a href="<?php $controller='exam'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-pencil-square-o"></em>
-                   <span>Exam</span>
-                </a>
-          </li>
-          <li class="<?php if($curr_url == 'test'){echo 'active';}    ?>">
-                <a href="<?php $controller='test'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-pencil"></em>
-                   <span>Test</span>
-                </a>
-          </li>
-          <li class="<?php if($curr_url == 'attendance'){echo 'active';} ?>">
-                <a href="<?php $controller='attendance'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-check"></em>
-                   <span>Attendance</span>
-                </a>
-          </li>
-          <li class="<?php if($curr_url == 'leave'){echo 'active';}    ?>">
-                <a href="<?php $controller='leave'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-clock-o"></em>
-                   <span>Leave</span>
-                </a>
-          </li>
-          <li class="<?php if($curr_url == 'announcement'){echo 'active';}    ?>">
-                <a href="<?php $controller='announcement'; 
-                   echo ADMIN_BASE_URL . $controller ?>">
-                   <em class="fa fa-bullhorn"></em>
-                   <span>Announcement</span>
-                </a>
-          </li>
-        <?php } ?>
        </ul>
        <!-- END sidebar nav-->
     </nav>
